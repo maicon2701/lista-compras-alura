103b5297e658d9cb9690beca8d0faf1d36c75f6e
maicon-at-235719508298 <maicond2701@gmail.com>
2025-09-16 21:52:26 -0300

    feat: Criando a função de adicionar um novo item em nossa lista

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-const inputItem = document.getElementById("input-item")
+const inputItem = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras")
+const botaoAdicionar = document.getElementById('adicionar-item');
 
-console.log(inputItem.value)
+let contador = 0;
+
+/*Evento para pegar o que foi escrito no input e enviar*/
+//Arrow function: () =>{}
+botaoAdicionar.addEventListener("click",(evento) => { /*FUNÇÃO PARA ADICIONAR ITEM NA LISTA*/
+    //prevent defaul: previne que a ação padrão do envio do evento aconteça, ou seja, ele não vai atualizar a tela correndo o risco de perder tudo!!!
+    evento.preventDefault();
+    //o === está sendo usado como comparador. No caso ele está comparando se o valor é igual a vazio ""
+    if (inputItem.value === ""){
+        alert("O campo está vazio, por favor insira um item!")
+        //O return utilizado sozinho, faz com que ação que estava sendo feita, retorne. Ou seja, ela não executa
+        return;
+    }
+
+    //Cria um elemento novo (li) e armazena ele dentro da variavel
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    //Adiciona a classe CSS nessa variavel(que contem a tag html div). Sem isso quando adicionássemos esse item na lista, ele não viria com as mesmas estilizações do item já estabelcido.
+    containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    //Estamos definindo o tipo desse input para checkbox
+    inputCheckbox.type = "checkbox";
+    //Define o id desse inputa e adiciona valor ao contador
+    inputCheckbox.id = "checkbox-" + contador++;
+    const nomeItem = document.createElement("p");
+    //O innerText altera o texto do elemento
+    nomeItem.innerText = inputItem.value;
+    //Atribui no elemento pai (containerItem) os elementos filhos (input e item). Basicamente estamos fazendo uma conexão entre as tags div,input e p
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeItem);
+    itemDaLista.appendChild(containerItemDaLista);
+    /*Fizemos uma conexão entre elementos pai e filho de tras para frente. Isso significa que LI é o pai da div, a div é pai do input e da tag p.*/
+    
+    //Adicionamos a nossa li na tag ul. Isso faz com que todo o que foi criado anteriormente seja anexado na nossa lista e mostrado em nossa tela.
+    listaDeCompras.appendChild(itemDaLista)
+
+});
