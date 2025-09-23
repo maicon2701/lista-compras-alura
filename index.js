5fe0badd003379c40e33d69ca6086341daa0a663
maicon-at-235719508298 <maicond2701@gmail.com>
2025-09-22 21:39:53 -0300

    feat: adicionando campos de data

--- index.js
+++ index.js
@@ -35,7 +35,28 @@ botaoAdicionar.addEventListener("click",(evento) => { /*FUNÇÃO PARA ADICIONAR 
     itemDaLista.appendChild(containerItemDaLista);
     /*Fizemos uma conexão entre elementos pai e filho de tras para frente. Isso significa que LI é o pai da div, a div é pai do input e da tag p.*/
     
+
+    const diaDaSemana = new Date().toLocaleDateString("pt-BR", { //O to localeDateString é uma metodo Date instances que retorna uma string que é language-sensitive, que representa a data no fuso horario local.  
+        //Long: É o que vai fazer o nome do dia sair completo, por ex: Segunda-feira.
+        weekday: "long"});
+
+    const data = new Date().toLocaleDateString("pt-BR");
+
+     //Funciona da mesma forma que toLocaleDateString, mas para o tempo. Ele transforma uma hora como "14:30:00" em "2:30 PM" ou "14:30", dependendo do formato que você escolher.
+    const hora  = new Date().toLocaleTimeString("pt-br", {
+        hour: "numeric",
+        minute: "numeric"
+    }
+
+    );
+    const dataCompleta = `${diaDaSemana} (${data}) àS ${hora}`;
+    const itemData = document.createElement('p');
+    itemData.innerText = dataCompleta;
+    itemData.classList.add("texto-data")
+    itemDaLista.appendChild(itemData);
+
     //Adicionamos a nossa li na tag ul. Isso faz com que todo o que foi criado anteriormente seja anexado na nossa lista e mostrado em nossa tela.
     listaDeCompras.appendChild(itemDaLista)
-
+    //Informa o valor do tempo atual(Tempo em que o evento foi chamado.)
+    
 });
